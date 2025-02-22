04256f5991adbe1c2c8ff5c7f6e55edeb911da40
Aj Tech Developer <techdeveloper.aj@gmail.com>
2025-02-22 15:46:53 +0530

    Initial code changes

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def main():
         bytes_data = uploaded_file.getvalue()
         with open(uploaded_file.name, "wb") as file:
             file.write(bytes_data)
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+        st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
         scenario = image_to_text(uploaded_file.name)
         story = story_generator(scenario)
         text_to_audio(story)
